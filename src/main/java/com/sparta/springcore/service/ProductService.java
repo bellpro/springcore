168669284bc5f614edaad1896b5a4d5c6e4e0989
bellpro <kju1909020@gmail.com>
2021-12-03 19:19:24 +0900

    회원별 상품 등록 및 조회

--- src/main/java/com/sparta/springcore/service/ProductService.java
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -1,8 +1,8 @@
 package com.sparta.springcore.service;
 
-import com.sparta.springcore.model.Product;
-import com.sparta.springcore.dto.ProductRequestDto;
 import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
 import com.sparta.springcore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,9 +19,9 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(ProductRequestDto requestDto) {
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
 // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
+        Product product = new Product(requestDto, userId);
 
         productRepository.save(product);
 
@@ -39,9 +39,8 @@ public class ProductService {
         return product;
     }
 
-    public List<Product> getProducts() {
-        List<Product> products = productRepository.findAll();
-
-        return products;
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
     }
 }
