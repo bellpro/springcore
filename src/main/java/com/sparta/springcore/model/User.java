751067f84ac030d424e638caa0d7be6b09ddfbd2
bellpro <kju1909020@gmail.com>
2021-12-03 21:37:46 +0900

    카카오 사용자 정보로 회원가입 구현

--- src/main/java/com/sparta/springcore/model/User.java
+++ src/main/java/com/sparta/springcore/model/User.java
@@ -29,13 +29,25 @@ public class User {
     private String email;
 
     @Column(nullable = false)
-    @Enumerated(value = EnumType.STRING)    // DB 저장시 Enum이 아닌 문자열로 저장
+    @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
+
+    @Column(unique = true)
+    private Long kakaoId;
 
     public User(String username, String password, String email, UserRoleEnum role) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.role = role;
+        this.kakaoId = null;
+    }
+
+    public User(String username, String password, String email, UserRoleEnum role, Long kakaoId) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.kakaoId = kakaoId;
     }
 }
