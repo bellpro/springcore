1eb6b04b8822ed95bd50f35643454548cc5b3b51
bellpro <kju1909020@gmail.com>
2021-12-03 20:19:24 +0900

    접근 불가 페이지

--- src/main/java/com/sparta/springcore/model/UserRoleEnum.java
+++ src/main/java/com/sparta/springcore/model/UserRoleEnum.java
@@ -1,6 +1,21 @@
 package com.sparta.springcore.model;
 
-public enum UserRoleEnum {  // 값을 한정
-    USER, // 사용자 권한
-    ADMIN // 관리자 권한
+public enum UserRoleEnum {
+    USER(Authority.USER), // 사용자 권한
+    ADMIN(Authority.ADMIN); // 관리자 권한
+
+    private final String authority;
+
+    UserRoleEnum(String authority) {
+        this.authority = authority;
+    }
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
 }
