031a8d501b3ac31af041a12d44e9e9f4c02a5143
bellpro <kju1909020@gmail.com>
2021-12-03 15:59:57 +0900

    Controller, Service, Repository 리팩토링

--- src/main/java/com/sparta/springcore/controller/ProductController.java
+++ src/main/java/com/sparta/springcore/controller/ProductController.java
@@ -1,25 +1,27 @@
-package com.sparta.springcore;
+package com.sparta.springcore.controller;
 
-import lombok.RequiredArgsConstructor;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController() {
-        ProductService productService = new ProductService();
+    @Autowired
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     // 신규 상품 등록
     @PostMapping("/api/products")
-    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
         Product product = productService.createProduct(requestDto);
 
 // 응답 보내기
@@ -28,7 +30,7 @@ public class ProductController {
 
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         Product product = productService.updateProduct(id, requestDto);
 
 // 응답 보내기 (업데이트된 상품 id)
@@ -37,7 +39,7 @@ public class ProductController {
 
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts() throws SQLException {
+    public List<Product> getProducts() {
         List<Product> products = productService.getProducts();
 
 // 응답 보내기
